9bca7a22a71e62520a1d70f0504da73389535629
Kyle Pawlowski <52301345+hottshott28@users.noreply.github.com>
2020-07-14 23:52:59 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+// const welcome = {
+//   greeting: "Hey",
+//   title: 'React'
+// };
+
+function getTitle(title) {
+  return title;
+}
 function App() {
+
+
+  // do something in between 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
