da04cece330b0d67a7e044d3d1a4777ba632c189
Kyle Pawlowski <52301345+hottshott28@users.noreply.github.com>
2020-10-26 20:38:43 -0600

    page 33, props

--- src/App.js
+++ src/App.js
@@ -1,54 +1,77 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-]
-function App () {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleSearch = event => {
+    console.log(event.target.value);
+  };
+
+  
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search onSearch={handleSearch}/>
 
       <hr />
 
-      <List />
+      <List list={stories} />
+    </div>
+  )};
+
+const Search = props => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+    
+    props.onSearch(event);
+  };
+
+  
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p> 
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+      
     </div>
   );
-}
+};
 
-function List() {
-  return list.map(function(fuck) {
+const List = props => 
+  props.list.map(fuck => {
     return (
       <div key={fuck.objectID}>
       <span>
         <a href={fuck.url}>{fuck.title}</a>
       </span>
-      <br></br>
       <span>{fuck.author}</span>
-      <br></br>
       <span>{fuck.num_comments}</span>
-      <br></br>
       <span>{fuck.points}</span>
       </div>
 
     )
-  })
-}
+  });
 export default App;
